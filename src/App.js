660058eed0319d95eb7f1944d998fe442fa07174
sunny mishra <sunnykumarmishra6@gmail.com>
2020-09-04 20:13:13 -0700

    second change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There, I m sunny
+          Hi There, I m sunny ,How r u
        </p>
         <a
           className="App-link"
